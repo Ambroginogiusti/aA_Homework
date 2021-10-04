d37615cd2159d37d3a0490eb1da4128fca6c004a
Ambrogino Giusti <ambroginogiusti@gmail.com>
2021-10-04 14:49:40 -0700

    Work on project(s)

---
+++ W4D1/graph_nodes.rb
@@ -0,0 +1,22 @@
+class GraphNode
+
+end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
+
+def bfs(starting_node, target_value)
+
+end
+
+# bfs(a, "b")
+
+# bfs(a, "f")
